1dbebc4496b580e24e9daec7c5079bb8be0559a7
awaghadpd <74618130+awaghadpd@users.noreply.github.com>
2023-12-14 14:49:28 +0530

    Update and rename configure-ingress-controller.sh to configure-test-portal.sh

---
+++ test-folder/configure-test-portal.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+NAMESPACE=$1
+INGRESS_NAME=$2
+SERVICE_NAME=$3
+DEPLOYMENT_NAME=$4
+
+
+
+
+
+
+# create namespace if doesn't exists
+kubectl create namespace $NAMESPACE --dry-run=client -o yaml | kubectl apply -f -
+
+# Create aks-helloworld and ingress-demo
+kubectl apply -f $INGRESS_NAME --namespace $NAMESPACE
+kubectl apply -f $SERVICE_NAME --namespace $NAMESPACE
+kubectl apply -f $DEPLOYMENT_NAME --namespace $NAMESPACE
+
+ 
